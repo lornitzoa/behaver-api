86c7df6bc66060ccf3d7e3a600bc60bff96df330
Anna Lornitzo <lornitzoa@gmail.com>
2019-09-12 14:56:03 -0400

    register route fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :users do
     collection do
       post '/login', to: 'users#login'
+
+      post '/register', to: 'users#register'
     end
-  end 
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
